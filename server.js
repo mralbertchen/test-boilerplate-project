78bf05c26eab287643e7f04f6dee598649a50340
Albert Chen <albert.chen@me.com>
2018-09-08 11:44:31 -0700

    Added and tested Express API backend

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('./'));
 app.use(express.static('dist'));
+app.use(bodyParser.json());
+
+app.get('/api/:p', (req, res) => {
+  const { p } = req.params;
+  res.status(200).json({ parameter: p });
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
 
 app.get('*', (req, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
 });
+
 
 const port = process.env.PORT || 3000;
 
