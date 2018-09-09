1c9acb3b2ca684eca71b4bfa4134ae3344a9792d
Albert Chen <albert.chen@me.com>
2018-09-09 11:50:11 -0700

    Testing Reactstrap

--- src/app/components/insert/Insert.jsx
+++ src/app/components/insert/Insert.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Progress, Col, Row } from 'reactstrap';
 
-const Insert = () => {
-  return (
-    <div className="container insert">
-      <h1>Home</h1>
-      <Button color="danger">Danger</Button>
-    </div>
-  )
+class Insert extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { progress: 0 };
+
+    this.addFive = this.addFive.bind(this);
+    this.minusFive = this.minusFive.bind(this);
+  }
+
+  addFive() {
+    const newProgress = this.state.progress + 5 > 100 ? 0 : this.state.progress + 5;
+    this.setState({
+      progress: newProgress,
+    });
+  }
+
+  minusFive() {
+    const newProgress = this.state.progress - 5 < 0 ? 0 : this.state.progress - 5;
+    this.setState({
+      progress: newProgress,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container insert">
+        <Row>
+          <h1>Testing Reactstrap</h1>
+        </Row>
+        <Row>
+          <Col>
+            <Button color="primary" onClick={this.addFive}>
+              +5
+            </Button>
+          </Col>
+          <Col>
+            <Button color="danger" onClick={this.minusFive}>
+              -5
+            </Button>
+          </Col>
+        </Row>
+          <Progress value={this.state.progress} />
+      </div>
+    );
+  }
 }
 
 export default Insert;
